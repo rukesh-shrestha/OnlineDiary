363d59f3a13d00e311a4c0b8923ed9af41901bbe
Rukesh Shrestha <54740124+rukesh-shrestha@users.noreply.github.com>
2021-09-17 20:04:21 +0545

    Update settings.py

--- DairyApp/settings.py
+++ DairyApp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e3p-8lqzy!q)m(*$+czwnc+grx0u257%!!mrii2u7s3=(%asy6'
+SECRET_KEY = 'e3p-8lqzy!q)m(*$+czwnc+grx0u257%!!mrii2u7s3=(%hgjhgasy6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
